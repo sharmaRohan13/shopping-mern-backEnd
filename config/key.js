3de4cfe3a7c84afa447d91321632612e25f00003
rohan.sharma@ggktech.com <rohan.sharma@ggktech.local>
2018-11-13 11:51:48 +0530

    final debugging

--- config/key.js
+++ config/key.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://rohan:rohan123@ds253243.mlab.com:53243/shopping-list-mern'
-
+  mongoURI:
+    "mongodb://rohan:rohan123@ds253243.mlab.com:53243/shopping-list-mern"
 };
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
 
-const items = require('./routes/api/items');
+const items = require("./routes/api/items");
 
 const app = express();
 
@@ -11,27 +11,34 @@ const app = express();
 app.use(bodyParser.json());
 
 //DB Config
-const db = require('./config/key').mongoURI;
+const db = require("./config/key").mongoURI;
 
 //Connect to Mongo
 mongoose
-    .connect(db)
-    .then(()=> console.log('MongoDB Connected..'))
-    .catch(err => console.log(err));
+  .connect(db)
+  .then(() => console.log("MongoDB Connected.."))
+  .catch(err => console.log(err));
 
-    //Use Routes
-    app.use('/api/items', items);
+//Use Routes
+app.use("/api/items", items);
 
-    //Serve static assets if in production
-    if(process.env.NODE_ENV === 'production'){
-        //Set Static folder
-        app.use(express.static('FrontEnd/build'));
+//Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //Set Static folder
+  app.use(express.static("../shopping-mern-frontEnd/build"));
 
-        app.get('*', (req, res) => {
-            res.sendFile(path.resolve(__dirname, 'FrontEnd', 'build', 'index.html'))
-        })
-    }
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(
+        __dirname,
+        "../shopping-mern-frontEnd",
+        "build",
+        "index.html"
+      )
+    );
+  });
+}
 
-    const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-    app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log(`Server started on port ${port}`));
